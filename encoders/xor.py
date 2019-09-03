4158c277b258a7fe383bdedf79697baf98f1d308
thenullsecxero <thexero@posteo.org>
2019-09-04 08:47:35 +1200

    updated asm

--- encoders/xor.py
+++ encoders/xor.py
@@ -12,8 +12,8 @@ stub8
 00401002     E8 FFFFFF      CALL OLLYDBG.00401006                    ; Push EIP
 00401006     FFC1           INC ECX                                  ; Increase ECX
 00401008     58             POP EAX                                  ; Get EIP
-00401009     B1 FF          MOV CL,0FF                               ; Mov length of shellcocde into CL
-0040100B     03C8           ADD ECX,EAX                              ; Add EIP to EXC
+00401009     B1 FF          MOV CL,0FF                               ; Mov length of shellcode into CL
+0040100B     03C8           ADD ECX,EAX                              ; Add EIP to ECX
 0040100D     8070 0F 0F     XOR BYTE PTR DS:[EAX+F],0F               ; XOR byte at EAX+0F with seed
 00401011     40             INC EAX                                  ; Increase EAX
 00401012     3BC1           CMP EAX,ECX                              ; Compare ECX to EAX
@@ -22,19 +22,20 @@ stub8
 33 C9 E8 FF FF FF FF C1 58 B1 FF 03 C8 80 70 0F 0F 40 3B C1 75 F7
 
 stub16
-00401000 >   33C9           XOR ECX,ECX
-00401002     E8 FFFFFF      CALL OLLYDBG.00401006
-00401006     FFC1           INC ECX
-00401008     66:B9 FF00     MOV CX,0FF
-0040100C     58             POP EAX
-0040100D     03C8           ADD ECX,EAX
-0040100F     8070 11 0F     XOR BYTE PTR DS:[EAX+11],0F
-00401013     40             INC EAX
-00401014     3BC1           CMP EAX,ECX
-00401016    ^75 F7          JNZ SHORT OLLYDBG.0040100F
+00401000 >   33C9           XOR ECX,ECX                              ; Zero ECX
+00401002     E8 FFFFFF      CALL OLLYDBG.00401006                    ; Push EIP
+00401006     FFC1           INC ECX                                  ; Increase ECX
+00401008     66:B9 FF00     MOV CX,0FF                               ; Mov length of shellcode into CX
+0040100C     58             POP EAX                                  ; Get EIP
+0040100D     03C8           ADD ECX,EAX                              ; Add EIP to ECX
+0040100F     8070 11 0F     XOR BYTE PTR DS:[EAX+11],0F              ; XOR byte at EAX+11 with seed
+00401013     40             INC EAX                                  ; Increase counter
+00401014     3BC1           CMP EAX,ECX                              ; Compare counter to length of shellcode
+00401016    ^75 F7          JNZ SHORT OLLYDBG.0040100F               ; If not, return to XOR
 
 33 C9 EB FF FF FF FF C1 66 B9 FF 00 58 03 C8 80 70 11 0F 40 3B C1 75 F7
 """
+
 from struct import pack
 from exploitutils import *
 
